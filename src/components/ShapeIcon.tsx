13524b51aee2d5e65b3e5e6c0072e06b60b47da1
Ely <ely.zucca@hey.com>
2023-03-31 20:04:27 +0200

    improve shape icon svgs

--- src/components/ShapeIcon.tsx
+++ src/components/ShapeIcon.tsx
@@ -10,20 +10,17 @@ const ShapeIcon: FC<Props> = ({ shape, size = 32 }) => {
     default:
     case 'triangle':
       return (
-        <svg width={size} height={size} viewBox="0 0 512 512">
+        <svg width={size} height={size} viewBox="0 0 37 32">
           <path
-            d="M278.313 48.296a42.667 42.667 0 0 1 15.876 15.876l182.478 319.336c11.691 20.46 4.583 46.523-15.876 58.214a42.667 42.667 0 0 1-21.169 5.621H74.667C51.103 447.343 32 428.241 32 404.677a42.667 42.667 0 0 1 5.622-21.169L220.099 64.172c11.691-20.46 37.755-27.567 58.214-15.876Z"
+            d="m16.53.99L.27,28.96c-.78,1.33.19,3.01,1.73,3.01h32.51c1.54,0,2.5-1.67,1.73-3.01L19.98.99c-.77-1.33-2.69-1.33-3.46,0Z"
             fill="#57b3da"
           />
         </svg>
       )
     case 'square':
       return (
-        <svg width={size} height={size} viewBox="0 0 24 24">
-          <path
-            d="M4.281 3h16.437A1.281 1.281 0 0 1 22 4.281v16.437A1.282 1.282 0 0 1 20.718 22H4.282A1.282 1.282 0 0 1 3 20.718V4.281A1.281 1.281 0 0 1 4.281 3z"
-            fill="#ffbb10"
-          />
+        <svg width={size} height={size} viewBox="0 0 32 32">
+          <rect width={32} height={32} rx={2} ry={2} fill="#ffbb10" />
         </svg>
       )
     case 'circle':
