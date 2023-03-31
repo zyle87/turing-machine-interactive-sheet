2b2821b4c5228e479840a9317f33a85914543fb0
Ely <ely.zucca@hey.com>
2023-03-31 20:08:29 +0200

    replace shape icon size by size multplier

--- src/components/ShapeIcon.tsx
+++ src/components/ShapeIcon.tsx
@@ -2,15 +2,15 @@ import { FC } from 'react'
 
 type Props = {
   shape: Shape
-  size?: number
+  sizeMultiplier?: number
 }
 
-const ShapeIcon: FC<Props> = ({ shape, size = 32 }) => {
+const ShapeIcon: FC<Props> = ({ shape, sizeMultiplier = 1 }) => {
   switch (shape) {
     default:
     case 'triangle':
       return (
-        <svg width={size} height={size} viewBox="0 0 37 32">
+        <svg width={37 * sizeMultiplier} height={32* sizeMultiplier} viewBox="0 0 37 32">
           <path
             d="m16.53.99L.27,28.96c-.78,1.33.19,3.01,1.73,3.01h32.51c1.54,0,2.5-1.67,1.73-3.01L19.98.99c-.77-1.33-2.69-1.33-3.46,0Z"
             fill="#57b3da"
@@ -19,13 +19,13 @@ const ShapeIcon: FC<Props> = ({ shape, size = 32 }) => {
       )
     case 'square':
       return (
-        <svg width={size} height={size} viewBox="0 0 32 32">
+        <svg width={32* sizeMultiplier} height={32* sizeMultiplier} viewBox="0 0 32 32">
           <rect width={32} height={32} rx={2} ry={2} fill="#ffbb10" />
         </svg>
       )
     case 'circle':
       return (
-        <svg width={size} height={size} viewBox="0 0 32 32">
+        <svg width={32* sizeMultiplier} height={32* sizeMultiplier} viewBox="0 0 32 32">
           <circle cx={16} cy={16} r={16} fill="#7f66ad" />
         </svg>
       )
