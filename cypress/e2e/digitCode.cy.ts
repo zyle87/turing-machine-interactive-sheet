4f3d3280a281bfff94485e20657f31e83a5871d4
Zyle <ely.zucca@hey.com>
2023-04-06 14:31:43 +0200

    add some e2e snapshots

--- cypress/e2e/digitCode.cy.ts
+++ cypress/e2e/digitCode.cy.ts
@@ -1,37 +1,25 @@
-// describe('digitCode', () => {
-//   it('should match digit code', () => {
-//     cy.wrap(
-//       Cypress.automation('remote:debugger:protocol', {
-//         command: 'Emulation.setDeviceMetricsOverride',
-//         params: {
-//           deviceScaleFactor: 1,
-//           width: 0,
-//           height: 0,
-//           mobile: false,
-//         },
-//       })
-//     )
+describe('digitCode', () => {
+  it('should match digit code', () => {
+    cy.visit('/').wait(1000)
 
-//     cy.visit('/').wait(1000)
+    cy.get('#digit-code__triangle-1-button').click()
+    cy.get('#digit-code__triangle-2-button').click()
+    cy.get('#digit-code__triangle-3-button').click()
+    cy.get('#digit-code__triangle-3-button').click()
+    cy.get('#digit-code__triangle-4-button').click()
+    cy.get('#digit-code__square-1-button').click()
+    cy.get('#digit-code__square-2-button').click()
+    cy.get('#digit-code__square-2-button').click()
+    cy.get('#digit-code__square-5-button').click()
+    cy.get('#digit-code__circle-2-button').click()
+    cy.get('#digit-code__circle-3-button').click()
+    cy.get('#digit-code__circle-4-button').click()
+    cy.get('#digit-code__circle-4-button').click().blur()
 
-//     cy.get('#digit-code__triangle-1-button').click()
-//     cy.get('#digit-code__triangle-2-button').click()
-//     cy.get('#digit-code__triangle-3-button').click()
-//     cy.get('#digit-code__triangle-3-button').click()
-//     cy.get('#digit-code__triangle-4-button').click()
-//     cy.get('#digit-code__square-1-button').click()
-//     cy.get('#digit-code__square-2-button').click()
-//     cy.get('#digit-code__square-2-button').click()
-//     cy.get('#digit-code__square-5-button').click()
-//     cy.get('#digit-code__circle-2-button').click()
-//     cy.get('#digit-code__circle-3-button').click()
-//     cy.get('#digit-code__circle-4-button').click()
-//     cy.get('#digit-code__circle-4-button').click().blur()
+    cy.get('#digit-code-section').matchImageSnapshot('digit code filled')
 
-//     cy.get('#digit-code-section').matchImageSnapshot('filled')
+    cy.get('#digit-code__clear-button').click().blur()
 
-//     cy.get('#digit-code__clear-button').click().blur()
-
-//     cy.get('#digit-code-section').matchImageSnapshot('cleared')
-//   })
-// })
+    cy.get('#digit-code-section').matchImageSnapshot('digit code cleared')
+  })
+})
--- cypress/e2e/rounds.cy.ts
+++ cypress/e2e/rounds.cy.ts
@@ -1,25 +1,13 @@
-// describe('rounds', () => {
-//   it('should add a round and undo it', () => {
-//     cy.wrap(
-//       Cypress.automation('remote:debugger:protocol', {
-//         command: 'Emulation.setDeviceMetricsOverride',
-//         params: {
-//           deviceScaleFactor: 1,
-//           width: 0,
-//           height: 0,
-//           mobile: false,
-//         },
-//       })
-//     )
+describe('rounds', () => {
+  it('should add a round and undo it', () => {
+    cy.visit('/').wait(1000)
 
-//     cy.visit('/').wait(1000)
+    cy.get('#rounds__add-round-button').click().blur()
 
-//     cy.get('#rounds__add-round-button').click().blur()
+    cy.get('#rounds-section').matchImageSnapshot('rounds with 1 empty round')
 
-//     cy.get('#rounds-section').matchImageSnapshot('with 1 empty round')
+    cy.get('#rounds__delete-round-1-button').click()
 
-//     cy.get('#rounds__delete-round-1-button').click()
-
-//     cy.get('#rounds-section').matchImageSnapshot('cleared')
-//   })
-// })
+    cy.get('#rounds-section').matchImageSnapshot('rounds cleared')
+  })
+})
