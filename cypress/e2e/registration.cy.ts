a1f56537610cf6632617af82a2a8c1d17f295552
Zyle <ely.zucca@hey.com>
2023-04-06 11:47:03 +0200

    update e2e snapshots

--- cypress/e2e/registration.cy.ts
+++ cypress/e2e/registration.cy.ts
@@ -17,11 +17,11 @@ describe('registration', () => {
     cy.get('#registration__name-text-field').type('john doe')
     cy.get('#registration__hash-text-field').type('B62 0GL M').blur()
 
-    cy.get('#registration-section').matchImageSnapshot('filled')
+    cy.matchImageSnapshot('filled')
 
     cy.get('#registration__name-text-field__clear-button').click()
     cy.get('#registration__hash-text-field__clear-button').click()
 
-    cy.get('#registration-section').matchImageSnapshot('cleared')
+    cy.matchImageSnapshot('cleared')
   })
 })
